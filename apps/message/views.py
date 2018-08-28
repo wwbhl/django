201b8f7077ab9474b422ca418906c5326d889188
WeatherBaren <baohailong@sina.com>
2018-08-28 13:33:23 +0800

    第3章 通过留言板功能回顾 django 基础知识

---
+++ apps/message/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+def getform(request):
+    return render(request, 'message_form.html')
